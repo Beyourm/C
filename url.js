4774f72a868b5a56dba4884cd8e43ffbcda5e333
Beyourm <almushkibdullah@gmail.com>
2025-10-18 02:19:36 +0300

    Update url.js

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-const URL_LONG = "https://script.google.com/macros/s/AKfycbyp2XBgfpHrsA70OLMG3gK0HC6C6lMDhio9mCn3nmbIDFpkPTz1LUI7-UiCllkZ5akr/exec";
+const URL_LONG = "https://script.google.com/macros/s/AKfycbzvPX_JMZAPCQJHPKicmB-ULX1ZStQYPJNO1ktvAr8MFVLecFSs3oyL0M9P17Jo3MtN/exec";
